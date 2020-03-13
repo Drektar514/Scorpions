61d7572263f7f41400db1d4d31475b95d617a6d7
Drektar514 <62055563+Drektar514@users.noreply.github.com>
2020-03-13 14:46:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,27 +8,21 @@ namespace HomeWork_1
 {
     class Program
     {
-    
-
         static void Main(string[] args)
         {
-
             byte firstVariable;
             float temperature = 35.5f;
             char sos = 'S';
             bool pizzaMaking = true;
             bool cutVegetables = false;
             string myName = "Sergey";
-            long Ants = 100000000000000;
+            long ants = 100000000000000;
             double gravity = 6.672d;
             int potatoNumber = 25;
             short channelCounter = 99;
             Console.WriteLine(channelCounter);
             Console.ReadKey();
         }
-
-     
-    }
-        
+    }     
 }
 
