575277cbbc6e03924a8fd9eaffb05873e2cea60a
Drektar514 <62055563+Drektar514@users.noreply.github.com>
2020-03-13 10:46:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+
+            byte firstVariable;
+            float temperature = 35.5f;
+            char sos = 'S';
+            bool pizzaMaking = true;
+            bool cutVegetables = false;
+            string myName = "Sergey";
+            long Ants = 100000000000000;
+            double gravity = 6.672d;
+            int potatoNumber = 25;
+            short channelCounter = 99;
+            Console.WriteLine(channelCounter);
+            Console.ReadKey();
+        }
+
+     
+    }
+        
+}
+
